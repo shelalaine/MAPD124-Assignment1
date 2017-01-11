122fa9c99fdada2b22b29d2d60141d62734140e5
shelalaine <schan211@my.centennialcollege.ca>
2017-01-11 14:56:20 -0500

    Set variables and methods of ViewController to private

--- MAPD124-Assignment1/ViewController.swift
+++ MAPD124-Assignment1/ViewController.swift
@@ -11,27 +11,30 @@ import UIKit
 class ViewController: UIViewController {
 
     // Indicate that no number / digit is pressed
-    var numberIsClicked = false
+    private var numberIsClicked = false
     
-    @IBOutlet weak var labelDisplay: UILabel!
+    @IBOutlet private weak var labelDisplay: UILabel!
 
     
     // 0 - 9 button pressed event handler
-    @IBAction func buttonClicked(_ sender: UIButton) {
+    @IBAction private func buttonClicked(_ sender: UIButton) {
         
         if (numberIsClicked) {
             // Append the number pressed to the right-most of the label
             labelDisplay.text = labelDisplay.text! + "\(sender.currentTitle!)"
         } else {
-            // Display the number pressed
-            labelDisplay.text = "\(sender.currentTitle!)"
-            // Indicate that a number is already pressed
-            numberIsClicked = true
+            // Display the number pressed if other than zero
+            if (sender.currentTitle! != "0") {
+                labelDisplay.text = "\(sender.currentTitle!)"
+                
+                // Indicate that a number is already pressed
+                numberIsClicked = true
+            }
         }
     }
 
     // AC button pressed event handler
-    @IBAction func buttonACClicked(_ sender: UIButton) {
+    @IBAction private func buttonACClicked(_ sender: UIButton) {
         // Set the display to 0
         labelDisplay.text = "0"
         // Reset the number clicked status
